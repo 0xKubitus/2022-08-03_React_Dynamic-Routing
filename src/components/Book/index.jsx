11a524d6c46eae6d6403635e39cea498e8063242
0xKubitus <n.cubilier@gmail.com>
2022-08-08 22:22:56 +0200

    adding some css

--- src/components/Book/index.jsx
+++ src/components/Book/index.jsx
@@ -6,12 +6,23 @@ import books from '../../data/books';
 
 const Book = () => {
   const { bookSlug } = useParams();
+
+  // Par défaut, j'indique que nous n'avons pas de livre sélectionné. 
   const [currentBook, setCurrentBook] = useState(undefined);
 
+  // Dès que le composant est monté ou que l'URL change, j'appelle setCurrentBook en lui passant le livre, grâce au useEffect().
   useEffect(() => {
     const foundBook = books.find((book) => book.slug === bookSlug);
     setCurrentBook(foundBook);
   }, [bookSlug])
+
+// Si currentBook n'est pas undefined, alors je retourne le contenu du livre. 
+// Sinon, je retourne un paragraphe d'erreur.
+
+  //const bookContent = currentBook !=== undefined ? 
+
+// Alors, si currentBook contient bien un livre, je renvoie toutes ses informations. Sinon je mets un message d'erreur. Cela nous permet par exemple de ne pas avoir de bug, dans le cas où l'utilisateur rentrerait lui-même une URL personnalisée.
+
 
   const booksLinks = books.map( (book) => {  
     return (
@@ -23,11 +34,15 @@ const Book = () => {
     );
   });
 
-
   
   return (
-    <div className="booklinks-main">
-      <div className="booklinks-sub">{booksLinks}</div>
+    <div className="books-main">
+      <div className="booklinks">{booksLinks}</div>
+
+      <div className="book-content">
+        <p id="book-title"><b><u>titre</u></b>, auteur</p>
+        <p id="book-desc">description</p>
+      </div>
     </div>
   )
 };
